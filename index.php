ad517ae9706dfdb56f4ba090555dfb97741a2fc7
sssnakes <finnan.barry@gmail.com>
2016-11-09 18:21:31 +0100

    initial layout, no illus

---
+++ index.php
@@ -0,0 +1,193 @@
+<html>
+<head>
+
+  <title>Finnán Barry</title>
+  <meta name="application-name"     content="Finnán Barry">
+  <meta name="viewport"             content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="js/jquery-2.2.0.min.js"></script>
+  <script src="js/jquery-ui.min.js"></script>
+  <!-- <script type="text/javascript" src="./js/app.js"></script> -->
+
+</head>
+<body>
+
+  <div class="ill-button"><span class="red">*</span>[illus.]</div>
+
+  <div class="container">
+
+    <div class="box br2">
+      <div class="left">
+        <p>#</p>
+      </div>
+      <div class="right">
+        <p>Finnán Barry</p>
+        <p>Designer &amp; Developer</p>
+        <p class="italic">[Éire; fin – aw – n]</p>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left"></div>
+      <div class="right">
+        <p>Hi there. I’m a working designer &amp; developer in Amsterdam, touching my hand on most aspects of the process, from research through to design and production. I also used do illustration work<span class="red">*</span>on the side, which I don’t do as much of any more, but the skills do still come in handy from time to time.</p>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left">—</div>
+      <div class="right">
+        <p>For the past year, I’ve been teaching myself how to code, mainly web, as a means to better understand the the underlying structures and methodologies of programming, as well as getting to grips with the freelance world to see the business side of things.</p>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left">—</div>
+      <div class="right">
+        <p>Right before the summer I was also interning under <a href="http://www.jamesbryangraves.com">James</a> of <a href="http://www.hackersanddesigners.com">Hackers and Designers</a>, and later attended their 2016 Summer Academy, which was great fun.</p>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left">—</div>
+      <div class="right">
+        <p>As of late, I’ve been getting to grips with PHP and the <a href="http://www.getkirby.com">Kirby CMS</a>, learning how to better structure my content (and, if I’m being honest with myself, a sidestep to postpone the inevitable diving into the MySQL world).</p>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left"></div>
+      <div class="right">
+        <p>You’ll find some links to my most recent projects below. If you have any questions, do send me a message and I’ll be happy to reply to you as soon as I can!</p>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left"></div>
+      <div class="right">
+        <p>CV available on request.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left"></div>
+      <div class="right">
+        <a style="display: block" href="http://www.twitter.com/finnanbarry">Twitter</a>
+        <a href="mailto:finnan.barry@gmail.com">Email</a>
+      </div>
+    </div>
+
+    <div class="box br">
+      <div class="left"></div>
+      <div class="right">
+        <p>Recent Works, in chronological order:</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Music 2016</p>
+        <a class="blue" href="http://www.finnanbarry.com/music/2016">./music/2016</a>
+        <br /><br />
+        <p>My second attempt an annual milestone micro-site to test my design practices and technical web-skills. Uses an ajax refresher script to load content into sections.</p>
+        <p>Destop only.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Rachel Sato Banks</p>
+        <a class="blue" href="http://www.rachelsatobanks.com">rachelsatobanks.com</a>
+        <br /><br />
+        <p>A portfolio site created for a photographer and copywriter, who often changes her role between the two, so we settled on a design that can allow the user to decide the degree to which they want to view of either.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">###</div>
+      <div class="right">
+        <p>Let it Grow</p>
+        <a class="blue" href="http://www.letitgrow.org">letitgrow.org</a>
+        <br /><br />
+        <p>Iconset created for the Let it Grow foundation, themed around leaves and plants.<br />
+        Made with <a href="http://gemmacope.land/">Gemma</a>.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Hackers &amp; Designers<br /> Summer Academy 2016</p>
+        <a class="blue" href="http://www.hackersanddesigners.com">hackersanddesigners.com</a>
+        <br /><br />
+        <p>Micro gag-site created for the Hackers &amp; Designers Summer Academy 2016. The 2016 theme of the event was ‘Immatieral Labour’, so we created a match-two game that ‘rewards’ the player with signup ads. <br /><br />Click the coin once you're through the link.
+        <br />Made with Rens and the H&amp;D team.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Weave &amp; Clay</p>
+        <p class="blue nope">weaveandclay.org</p>
+        <p>(Still in development)</p>
+        <br />
+        <p>Part shop, part design blog, a site celebrating contemporary Textile and Ceramic works from around the globe. Still in development currently, searching for a balance between the connecting with the artists and selling their works.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Mae&amp;Many</p>
+        <a class="blue" href="http://www.maeandmany.com">maeandmany.com</a>
+        <br /><br />
+        <p>A personal diary and travel blog made for a good friend. We wanted something incredibly modular, that Magot could edit herself if need be, so we went with a 'tweaked' version of the <a href="http://www.semplicelabs.com/">Semplice</a> theme for the end result.</p>
+        <p>Logo by <a href="http://www.fernandetfirmin.com/">Océane</a>.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Music 2015</p>
+        <a class="blue" href="http://www.finnanbarry.com/music/2015">./music/2015</a>
+        <br /><br />
+        <p>A personal milestone project, as I attempted at front-end development, using some basic jQuery scripts and the SoundJS library. Still rough around the edges, but notable to compare to the 2016 attempt.</p>
+        <p>Destop only.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">###</div>
+      <div class="right">
+        <p>BeamSystems</p>
+        <a class="blue" href="http://www.beamsystems.nl">beamsystems.nl</a>
+        <br /><br />
+        <p>Design assistance and Icon set created for an audiovisual production group, set to match Px Grotesk.</p>
+      </div>
+    </div>
+
+    <div class="box br2">
+      <div class="left">##</div>
+      <div class="right">
+        <p>Spring House</p>
+        <p class="blue nope">springhouse.nl</p>
+        <br />
+        <p>Microsite created for the launch of collaborative experimental workspace in Amsterdam. We wanted to give our audience an early look into the house, allowing them to explore each floor for themselves and experience a day within the building.</p>
+        <br />
+        <p>Since been replaced by a 'fuller' site, mirror <a href="http://www.finnanbarry.com/springhousemirror">here</a>.</p>
+        <p>Illustrations by <a href="http://www.rudyguedj.com/">Rudy</a>.</p>
+        <p>Copy by <a href="http://www.maeandmany.com/">Margot</a>.</p>
+      </div>
+    </div>
+
+
+
+  </div>
+
+</body>
+</html>
